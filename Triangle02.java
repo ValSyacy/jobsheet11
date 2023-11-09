3dd00499c55399a46369698008370c0210837a1d
ValSyacy <novalsyacy11@gmail.com>
2023-11-09 09:09:53 +0700

    Percobaan3

---
+++ Triangle02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Triangle02{
+    public static void main (String[] args){
+
+        Scanner input02 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = input02.nextInt();
+
+        int i = 0;
+        while(i <= N){
+            int j = 0;
+            while(j < i){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
